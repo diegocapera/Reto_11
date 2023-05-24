3e03d82d6abf1790430135ba32f5fe1f6538f01e
diegocapera <124608110+diegocapera@users.noreply.github.com>
2023-05-24 08:33:08 -0500

    Punto 4.py

--- Punto 4.py
+++ Punto 4.py
@@ -1,8 +1,8 @@
 # Punto 4
-# Pedir al usuario que ingrese el número de filas y columnas de la matriz
-filas = int(input("Ingrese el número de filas: "))
-columnas = int(input("Ingrese el número de columnas: "))
-# Crear una matriz vacía con los valores ingresados
+# Pedir al usuario que ingrese el numero de filas y columnas de la matriz
+filas = int(input("Ingrese el numero de filas: "))
+columnas = int(input("Ingrese el numero de columnas: "))
+# Crear una matriz vacia con los valores ingresados
 matriz = []
 for i in range(filas):
     fila = []
@@ -10,11 +10,11 @@ for i in range(filas):
         valor = int(input("Ingrese el valor para la fila {} y columna {}: ".format(i+1, j+1)))
         fila.append(valor)
     matriz.append(fila)
-# Pedir al usuario que ingrese el número de la columna que desea sumar
-columna_sumar = int(input("Ingrese el número de la columna que desea sumar (1 a {}): ".format(columnas)))
-# Verificar que la columna ingresada esté dentro del rango de columnas de la matriz
+# Pedir al usuario que ingrese el numero de la columna que desea sumar
+columna_sumar = int(input("Ingrese el numero de la columna que desea sumar (1 a {}): ".format(columnas)))
+# Verificar que la columna ingresada este dentro del rango de columnas de la matriz
 if columna_sumar < 1 or columna_sumar > columnas:
-    print("El número de columna ingresado no es válido")
+    print("El numero de columna ingresado no es valido")
 else:
     # Sumar los elementos de la columna seleccionada
     suma = 0
